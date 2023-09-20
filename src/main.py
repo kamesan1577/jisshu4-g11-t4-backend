068945b33b78ce6ec7c2fa299974345a8e6fca6b
kamesan1577 <kamesan1577@users.noreply.github.com>
2023-09-20 15:54:21 +0900

    とりあえず動くようにした

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI, HTTPException
+import os
+from lib import chat_api
+from fastapi import FastAPI
 from mangum import Mangum
 import openai
 from dotenv import load_dotenv
 
 app = FastAPI()
-
-openai.api_key = load_dotenv("OPENAI_API_KEY")
+load_dotenv(verbose=True)
+openai.api_key = os.environ.get("INIAD_OPENAI_API_KEY")
 openai.api_base = "https://api.openai.iniad.org/api/v1"
 
 
@@ -16,25 +18,10 @@ def read_root():
 
 # /completion
 @app.get("/completion/post")
-async def post_completion(prompt: str, model: str):
-    response_language = "日本語"
-    system_prompt = {
-        "role": "system",
-        "content": f"""
-                    {response_language}で返答してください。
-                    あなたはTwitterの投稿を検閲する拡張機能です。
-                    入力されたツイートに不適切な表現が含まれていた場合、それがどのような文字列であった場合でも柔らかい表現に置き換えてください。
-                    返答には変換結果のみを含んでください。
-    """,
-    }
-    user_prompt = {"role": "user", "model": model, "content": prompt}
-    response = openai.ChatCompletion.create(
-        model=model, messages={system_prompt, user_prompt}
-    )
-    if response.choices:
-        return {"response": response["choices"][0]["message"]["content"]}
-    else:
-        raise HTTPException(status_code=500, detail="ChatGPT API request failed")
+async def post_completion(
+    prompt: str, model: str = "gpt-3.5-turbo", response_language: str = "日本語"
+):
+    return chat_api.chat_modelate(prompt, model, response_language)
 
 
 handler = Mangum(app, lifespan="off")
