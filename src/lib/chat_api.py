1434cc079a384259fac7e00cc06a3272d46b058b
kamesan1577 <kamesan1577@users.noreply.github.com>
2023-09-29 02:08:33 +0900

    いい感じのログを出力するように

--- src/lib/chat_api.py
+++ src/lib/chat_api.py
@@ -1,8 +1,19 @@
 import openai
+import logging
+import json
 from fastapi import HTTPException
 
 
 def chat_modelate(prompt, user_id, model, response_language):
+    log_data = {
+        "user_id": user_id,
+        "prompt": prompt,
+        "model": model,
+        "response_language": response_language
+    }
+
+    logging.info(json.dumps(log_data))  # Logs the data in JSON format
+
     system_prompt = {
         "role": "system",
         "content": f"""
@@ -16,7 +27,19 @@ def chat_modelate(prompt, user_id, model, response_language):
     response = openai.ChatCompletion.create(
         model=model, messages=[system_prompt, user_prompt]
     )
+
     if response.choices:
-        return {"response": response["choices"][0]["message"]["content"]}
+        response_content = response["choices"][0]["message"]["content"]
+        response_log = {
+            "user_id": user_id,
+            "response_content": response_content
+        }
+        logging.info(json.dumps(response_log))  # Log the response in JSON format
+        return {"response": response_content}
     else:
+        error_log = {
+            "user_id": user_id,
+            "error": "ChatGPT API request failed"
+        }
+        logging.error(json.dumps(error_log))  # Log the error in JSON format
         raise HTTPException(status_code=500, detail="ChatGPT API request failed")
