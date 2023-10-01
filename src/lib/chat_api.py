771dbcfeeb177afb903312235ae703b3d89a6dd8
kamesan1577 <kamesan1577@users.noreply.github.com>
2023-10-02 01:31:42 +0900

    chat_apiのログレベルの変更

--- src/lib/chat_api.py
+++ src/lib/chat_api.py
@@ -3,6 +3,9 @@ import logging
 import json
 from fastapi import HTTPException
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 def chat_modelate(prompt, user_id, model, response_language):
     log_data = {
@@ -12,7 +15,7 @@ def chat_modelate(prompt, user_id, model, response_language):
         "response_language": response_language,
     }
 
-    logging.info(json.dumps(log_data))  # Logs the data in JSON format
+    logger.info(json.dumps(log_data))  # Logs the data in JSON format
 
     system_prompt = {
         "role": "system",
@@ -31,13 +34,13 @@ def chat_modelate(prompt, user_id, model, response_language):
         if response.choices:
             response_content = response["choices"][0]["message"]["content"]
             response_log = {"user_id": user_id, "response_content": response_content}
-            logging.info(json.dumps(response_log))  # Log the response in JSON format
+            logger.info(json.dumps(response_log))  # Log the response in JSON format
             return {"response": response_content}
         else:
             error_log = {"user_id": user_id, "error": "ChatGPT API request failed"}
-            logging.error(json.dumps(error_log))  # Log the error in JSON format
+            logger.error(json.dumps(error_log))  # Log the error in JSON format
             raise HTTPException(status_code=500, detail="ChatGPT API request failed")
     except Exception as e:
         error_log = {"user_id": user_id, "error": str(e)}
-        logging.error(json.dumps(error_log))
+        logger.error(json.dumps(error_log))
         raise HTTPException(status_code=500, detail="Runtime error")
