ca7666fac17b95e338a9ec1bb64fee95263a7dbd
kamesan1577 <kamesan1577@users.noreply.github.com>
2023-10-04 12:22:18 +0900

    んにゃぴ…

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ else:
     app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://omochifestival.com", "https://misskey.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
