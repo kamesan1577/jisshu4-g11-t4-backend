c9377b1c19421c7ef925f0d3f293a54dce282f84
kamesan1577 <kamesan1577@users.noreply.github.com>
2023-12-13 02:59:44 +0900

    #54 標準ライブラリでtimeline-safetyを非同期処理化
    並列処理と非同期処理の違いを言語化できていない
    Redisまわりをモジュール化した

--- src/lib/suggestion_api.py
+++ src/lib/suggestion_api.py
@@ -1,5 +1,10 @@
 import re
+import asyncio
+import hashlib
+import json
+from concurrent.futures import ThreadPoolExecutor
 from . import chat_api
+from .redis_client import RedisClient
 
 
 def is_required_moderation(prompt: str) -> bool:
@@ -18,6 +23,7 @@ def is_required_moderation(prompt: str) -> bool:
         flag = False
     return flag
 
+
 def get_safety_level(prompt: str) -> int:
     """
     文字列を受け取り、その安全性レベルを判定する
@@ -31,20 +37,46 @@ def get_safety_level(prompt: str) -> int:
     score = chat_api.get_safety_level(clean_text)
     return score
 
+
+# TODO キャッシングと判定処理の責任を分離したい
+async def async_get_safety_level(prompts: list[str])-> list[int]:
+    """
+    文字列のリストを受け取り、それらについての安全性レベルを非同期で判定する
+
+    Args:
+        prompts (list[str]): 判定対象の文字列のリスト
+    Returns:
+        list[int]: 安全性レベルのリスト（三段階、0が一番安全）
+    """
+    loop = asyncio.get_event_loop()
+    responses = [None] * len(prompts)
+
+    # キャッシュヒットしたプロンプトとしなかったプロンプトの順番がごちゃまぜにならないためのリスト
+    uncached_prompts = []
+    uncached_indices = []
+
+    for i, prompt in enumerate(prompts):
+        hash_key = hashlib.sha256((prompt + "safety_level").encode()).hexdigest()
+        cached = RedisClient.get_value(hash_key)
+        if cached:
+            responses[i] = json.loads(cached)["level"]
+        else:
+            uncached_prompts.append(prompt)
+            uncached_indices.append(i)
     
+    # キャッシュされていないプロンプトを非同期で処理する
+    if uncached_prompts:
+        with ThreadPoolExecutor() as executor:
+            tasks = [loop.run_in_executor(executor,chat_api.get_safety_level,prompt) for prompt in uncached_prompts]
+            uncached_responses = await asyncio.gather(*tasks)
 
-# def get_hidden_words(prompt: str):
-#     """文字列を受け取り、修正対象の単語のリストを返す
+        for index, response in zip(uncached_indices,uncached_responses):
+            hash_key = hash_key = hashlib.sha256((uncached_prompts[index] + "safety_level").encode()).hexdigest()
+            RedisClient.set_value(hash_key,json.dumps({"post": uncached_prompts[index], "level": response}),expire_time=60 * 60 * 24 * 7)
+            responses[index] = response
 
-#     Args:
-#         prompt (str): 判定対象の文字列
-#     Returns:
-#         list: 修正対象の単語のリスト
-#     """
-#     clean_text = _delete_html_tag(prompt)
-#     suggestions = morphological.ketaiso(clean_text, is_directry=False)
+    return responses
 
-#     return suggestions
 
 
 def _delete_html_tag(text: str):
--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
 import os
 import time
 import logging
-import hashlib
 import json
+import hashlib
 from .lib import chat_api, models, suggestion_api
-
+from .lib.redis_client import RedisClient
 # from .lib.db_client import db_instance
 from fastapi import FastAPI, HTTPException
 from starlette.middleware.cors import CORSMiddleware
 from mangum import Mangum
 from dotenv import load_dotenv
-import redis
+
 
 logger = logging.getLogger("PocLog")
 logger.setLevel(logging.INFO)
@@ -41,18 +41,6 @@ app.add_middleware(
 
 load_dotenv(verbose=True)
 
-
-REDIS_HOST = os.environ.get("REDIS_HOST")
-REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
-REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
-if REDIS_PASSWORD:
-    redis_client = redis.Redis(
-        host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD, db=0, ssl=True
-    )
-else:
-    redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
-
-
 # lambdaにデプロイすると動かない(そもそもいらない気がするけど)
 @app.get("/")
 def read_root():
@@ -71,41 +59,6 @@ async def post_completion(
     return {"response": response}
 
 
-# ツイートの修正が必要かどうかを判定(list[str])
-# @app.post("/moderations/suggestions")
-# async def post_suggestions(request: models.SuggestionsRequest):
-#     try:
-#         start_time = time.time()
-#         hash_key = hashlib.sha256((request.prompt + "suggestions").encode()).hexdigest()
-#         cached = redis_client.get(hash_key)
-
-#         if cached:
-#             hidden_words = json.loads(cached.decode("utf-8"))["suggestions"]
-#             # print("cache hit")
-#         else:
-#             # 修正対象の単語のリストを返す
-#             hidden_words = suggestion_api.get_hidden_words(request.prompt)
-#             redis_client.set(hash_key, json.dumps({"suggestions": hidden_words}))
-
-#         # 実行と同時にログに流す
-#         log = models.SuggestionsLog(
-#             user_id=request.user_id,
-#             post_id="hoge",
-#             original_text=request.prompt,
-#             hidden_texts=hidden_words,
-#         ).model_dump()
-#         try:
-#             logger.info(json.dumps(log))
-#             logger.debug(f"実行時間:{time.time() - start_time}秒")
-#         except Exception as e:
-#             logger.error(e)
-#             raise HTTPException(status_code=500, detail="Log send failed")
-#         return {"suggestions": hidden_words}
-#     except Exception as e:
-#         logger.error(e)
-#         raise HTTPException(status_code=500, detail="Suggestion failed")
-
-
 # ツイートの修正が必要かどうかを判定(boolean)
 @app.post("/moderations/suggestions/safety")
 async def judge_safety(request: models.SuggestionsRequest):
@@ -114,15 +67,15 @@ async def judge_safety(request: models.SuggestionsRequest):
         hash_key = hashlib.sha256(
             (request.prompt + "is_required_moderation").encode()
         ).hexdigest()
-        cached = redis_client.get(hash_key)
+
+        cached = RedisClient.get_value(hash_key)
 
         if cached:
-            flag = json.loads(cached.decode("utf-8"))["is_required_moderation"]
+            flag = json.loads(cached)["is_required_moderation"]
             logger.debug("cache hit")
         else:
             flag = suggestion_api.is_required_moderation(request.prompt)
-            redis_client.set(hash_key, json.dumps({"is_required_moderation": flag}))
-            redis_client.expire(hash_key, 60 * 60 * 24 * 7)
+            RedisClient.set_value(hash_key, json.dumps({"is_required_moderation": flag}),60 * 60 * 24 * 7)
 
         # 実行と同時にログに流す
         log = models.SafetyJudgementLog(
@@ -149,29 +102,16 @@ async def judge_safety_timeline_completion(
 ):
     try:
         start_time = time.time()
-        response = []
-        for post in request.prompts:
-            hash_key = hashlib.sha256((post + "safety_level").encode()).hexdigest()
-            cached = redis_client.get(hash_key)
-
-            if cached:
-                safety_level = json.loads(cached.decode("utf-8"))["level"]
-                logger.debug("cache hit")
-                response.append({"post": post, "level": safety_level})
-            else:
-                safety_level = suggestion_api.get_safety_level(post)
-                redis_client.set(
-                    hash_key, json.dumps({"post": post, "level": safety_level})
-                )
-                redis_client.expire(hash_key, 60 * 60 * 24 * 7)
-                response.append({"post": post, "level": safety_level})
-
-            try:
-                logger.debug(f"実行時間:{time.time() - start_time}秒")
-            except Exception as e:
-                logger.error(e)
-                raise HTTPException(status_code=500, detail="Log send failed")
-        return {"response": response}
+        safety_levels = await suggestion_api.async_get_safety_level(request.prompts)
+        
+        responses = [{"post": post, "level": safety_level} for post, safety_level in zip(request.prompts, safety_levels)]
+        try:
+            logger.debug(f"実行時間:{time.time() - start_time}秒")
+            # print(f"実行時間:{time.time() - start_time}秒")
+        except Exception as e:
+            logger.error(e)
+            raise HTTPException(status_code=500, detail="Log send failed")
+        return {"response": responses}
     except Exception as e:
         logger.error(e)
         raise HTTPException(status_code=500, detail="Safety judgement failed")
@@ -182,6 +122,7 @@ async def judge_safety_timeline_completion(
 async def judge_safety_timeline_moderation(
     request: models.TimeLineRequest,
 ):
+    # TODO 非同期処理に対応させる
     try:
         start_time = time.time()
         response = []
@@ -189,20 +130,16 @@ async def judge_safety_timeline_moderation(
             hash_key = hashlib.sha256(
                 (post + "safety_level_with_moderation_api").encode()
             ).hexdigest()
-            cached = redis_client.get(hash_key)
+            cached = RedisClient.get_value(hash_key)
 
             if cached:
-                safety_level = json.loads(cached.decode("utf-8"))["level"]
+                safety_level = json.loads(cached)["level"]
                 logger.debug("cache hit")
                 response.append({"post": post, "level": safety_level})
             else:
                 safety_level = int(suggestion_api.is_required_moderation(post))
-                redis_client.set(
-                    hash_key, json.dumps({"post": post, "level": safety_level})
-                )
-                redis_client.expire(hash_key, 60 * 60 * 24 * 7)
+                RedisClient.set_value(hash_key,json.dumps({"post": post, "level": safety_level}),expire_time=60 * 60 * 24 * 7)
                 response.append({"post": post, "level": safety_level})
-
             try:
                 logger.debug(f"実行時間:{time.time() - start_time}秒")
             except Exception as e:
@@ -212,100 +149,6 @@ async def judge_safety_timeline_moderation(
     except Exception as e:
         logger.error(e)
         raise HTTPException(status_code=500, detail="Safety judgement failed")
-
-
-# # TLの検閲
-# @app.post("/redaction")
-# async def post_redaction(
-#     request: models.TimeLineRequest,
-# ):
-#     try:
-#         start_time = time.time()
-#         response = []
-#         for post in request.prompts:
-#             # TODO コードが書き換わったときにキャッシュを消す
-#             hash_key = hashlib.sha256((post + "redaction").encode()).hexdigest()
-
-#             cached = redis_client.get(hash_key)
-
-#             if cached:
-#                 hidden_text = json.loads(cached.decode("utf-8"))["hidden"]
-#                 # print("cache hit")
-#                 response.append({"original": post, "hidden": hidden_text})
-#             else:
-#                 hidden_text = suggestion_api.get_hidden_words(post)
-#                 redis_client.set(hash_key, json.dumps({"hidden": hidden_text}))
-#                 response.append({"original": post, "hidden": hidden_text})
-#         logger.debug(f"実行時間:{time.time() - start_time}秒")
-#         return {"response": response}
-
-#     except Exception as e:
-#         logger.error(e)
-#         raise HTTPException(status_code=500, detail="Redaction failed")
-
-
-# # ツイートの修正を受け入れたかどうかをログに送信
-# @app.post("/poc/suggest-acceptance-collection")
-# async def post_is_accepted_suggestion(
-#     request: models.IsAcceptedSuggestionRequest,
-# ):
-#     log = models.IsAcceptedSuggestionLog(
-#         user_id=request.user_id,
-#         post_id="hoge",
-#         is_accepted=request.is_accepted,
-#         is_edited_by_user=request.is_edited_by_user,
-#         original_text=request.original_text,
-#         hidden_texts=request.hidden_texts,
-#     ).model_dump()
-#     try:
-#         logger.info(json.dumps(log))
-#         return {"message": "success", "is_accepted": request.is_accepted}
-#     except Exception as e:
-#         logger.error(e)
-#         raise HTTPException(status_code=500, detail="Log send failed")
-
-
-# @app.get("/moral-foundation/{sheet_name}/data")
-# async def get_moral_foundation_data(sheet_name: str):
-#     try:
-#         data = db_instance.fetch_sheet_value(sheet_name)
-#         return {"data": data}
-#     except db_instance.SheetNotFoundError:
-#         raise HTTPException(status_code=404, detail="Sheet not found")
-#     except Exception as e:
-#         logger.error(e)
-#         raise HTTPException(status_code=500, detail="Data fetch failed")
-
-
-# @app.post("/moral-foundation/{sheet_name}/data")
-# async def post_moral_foundation_data(sheet_name: str, data: models.Sheet):
-#     try:
-#         db_instance.add_sheet_value(sheet_name, data)
-#         return {"message": "success"}
-#     except db_instance.SheetNotFoundError:
-#         raise HTTPException(status_code=404, detail="Sheet not found")
-#     except Exception as e:
-#         logger.error(e)
-#         raise HTTPException(status_code=500, detail="Data post failed")
-
-
-# 隠された文字列の統計情報をログに送信
-# @app.post("/poc/hidden-text-collection")
-# async def post_hidden_text_collection(
-#     hidden_chars: models.HiddenChars,
-# ):
-#     log = models.SuggestionsLog(
-#         user_id=hidden_chars.user_id,
-#         post_id="hoge",
-#         original_text=hidden_chars.original_text,
-#         hidden_texts=hidden_chars.hidden_texts,
-#     ).model_dump()
-#     try:
-#         logger.info(json.dumps(log))
-#         return {"message": "success"}
-#     except Exception as e:
-#         logger.error(e)
-#         raise HTTPException(status_code=500, detail="Log send failed")
 
 
 handler = Mangum(app, lifespan="off")
